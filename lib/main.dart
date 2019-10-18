cbbc122f72e9bbc97715904953e9207497fb6dfc
3lVv0w <krseisenh@gmail.com>
2019-10-18 15:40:52 +0700

    code refactor

---
+++ lib/main.dart
@@ -0,0 +1,157 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import './cart.dart';
+// import './main_firebase.dart';
+import './mainscreen/product_card.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'KMITL',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'KMITL'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  final Color appColor = Colors.brown[200];
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomePage(),
+    CartPage(),
+    PlaceholderWidget(Colors.green)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: appColor,
+        title: new Text(
+          "CROP MART",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: HomePage(),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.white,
+        backgroundColor: appColor,
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.home), title: new Text("HOME")),
+          BottomNavigationBarItem(
+              icon: new Icon(Icons.shopping_cart), title: new Text("CART")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), title: new Text("PROFILE"))
+        ],
+      ),
+    );
+  }
+}
+
+class PlaceholderWidget extends StatelessWidget {
+  final Color color;
+
+  PlaceholderWidget(this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  Widget _buildCatList(String collection) {
+    return Container(
+      child: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection(collection).snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Text('Loading...');
+            default:
+              return ListView(
+                scrollDirection: Axis.horizontal,
+                children:
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                  return ProductCard(
+                    id: document.documentID,
+                    productName: document['productName'],
+                  );
+                }).toList(),
+              );
+          }
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(color: Colors.grey),
+      child: ListView(
+        children: <Widget>[
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                    padding: EdgeInsets.only(left: 10, top: 20),
+                    child: Text(
+                      "Category 1",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    )),
+                Container(
+                  height: 220,
+                  child: _buildCatList('crops'),
+                )
+              ]),
+          Divider(),
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                    padding: EdgeInsets.only(left: 10, top: 20),
+                    child: Text(
+                      "Category 2",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    )),
+                Container(
+                  height: 220,
+                  child: _buildCatList('crops'),
+                )
+              ]),
+        ],
+      ),
+    );
+  }
+}
--- lib/mainscreen/product_card.dart
+++ lib/mainscreen/product_card.dart
@@ -4,11 +4,15 @@ import '../product_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProductCard extends StatefulWidget {
-  ProductCard(BuildContext context, DocumentSnapshot document);
-  DocumentSnapshot document;
-//  final String productName;
-//  final int qty;
+  ProductCard(
+      {Key key,
+      this.id,
+      this.productName})
+      : super(key: key);
+ String productName;
+String id;
 //  final int price;
+
 
   @override
   State<StatefulWidget> createState() {
@@ -17,42 +21,43 @@ class ProductCard extends StatefulWidget {
 }
 
 class ProductCardState extends State<ProductCard> {
+  print(wid)
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new GestureDetector(
+    return Center(
+      child: GestureDetector(
         onTap: () {
           Navigator.push(
               context,
               MaterialPageRoute(
                   builder: (context) => ProductPage(
-                    productid: widget.document['productName'],
-                  )));
+                        productid: widget.productName,
+                      )));
         },
         child: Container(
           height: 200,
           width: 200,
           padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 10.0),
           margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 5),
-          decoration: new BoxDecoration(
+          decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(20),
           ),
-          child: new Column(
+          child: Column(
             children: <Widget>[
               Hero(
-                tag: 'product_picture' + widget.document['productName'],
+                tag: 'product_picture' + widget.productName,
                 child: Image.asset('images/gettyimages-580833893.jpg'),
               ),
               Column(children: <Widget>[
                 Text(
-                  widget.document['productName'],
+                  widget.productName,
                   style: TextStyle(fontSize: 20),
                 ),
                 Row(
                   children: <Widget>[
-                    Expanded(child: Text(widget.document['qty'].toString())),
-                    Expanded(child: Text(widget.document['price'].toString()))
+                    // Expanded(child: Text(widget.document['qty'].toString())),
+                    // Expanded(child: Text(widget.document['price'].toString()))
                   ],
                 )
               ]),
@@ -68,7 +73,7 @@ class ProductCardState extends State<ProductCard> {
         context,
         MaterialPageRoute(
             builder: (context) => ProductPage(
-              productid: '1',
-            )));
+                  productid: '1',
+                )));
   }
 }
